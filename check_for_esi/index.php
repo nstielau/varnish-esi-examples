a728285492a515754870900bf90f9ce62763d674
Nick Stielau <nick.stielau@gmail.com>
2012-09-11 21:48:51 +0000

    Fixing PHP warning.

--- check_for_esi/index.php
+++ check_for_esi/index.php
@@ -1,8 +1,9 @@
 <?php 
-echo "Surrogate Capabilities: " . $_SERVER['HTTP_SURROGATE_CAPABILITY'] . "\n";
 if (in_array('HTTP_SURROGATE_CAPABILITY', $_SERVER)) {
+  echo "Surrogate Capabilities: '" . $_SERVER['HTTP_SURROGATE_CAPABILITY'] . "'\n";
   echo "Looks like an upstream surrogate supports ESI";
 } else {
+  echo "Surrogate Capabilities: ''\n";
   echo "Looks like there is no ESI capable upstream";
 }
 ?>
